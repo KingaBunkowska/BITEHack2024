3e62dba6e0a59a09dd4213e7d64cc44306175164
MaciekBartoszNowakowski <macieknowakowski556@gmail.com>
2024-01-20 20:10:55 +0100

    Generator dungeonów

---
+++ src/main/java/oop/model/EncounterRoom.java
@@ -0,0 +1,11 @@
+package oop.model;
+
+public class EncounterRoom extends AbstractRoom{
+
+    public EncounterRoom(RoomType roomType, int column, int hight) {
+        super(roomType, column, hight);
+    }
+
+
+
+}
---
+++ src/main/java/oop/model/Main.java
@@ -0,0 +1,9 @@
+package oop.model;
+
+public class Main {
+    public static void main(String[] args) {
+        Dungeon dungeon = new Dungeon(8);
+        dungeon.representation();
+    }
+
+}
