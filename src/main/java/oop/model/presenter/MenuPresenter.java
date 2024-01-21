6db7b04f35fbed1f08cf86ea179e3c3d827b319f
MaciekBartoszNowakowski <macieknowakowski556@gmail.com>
2024-01-21 05:56:08 +0100

    Są panele. Jest wysoko

---
+++ src/main/java/oop/model/presenter/MenuPresenter.java
@@ -0,0 +1,21 @@
+package oop.model.presenter;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import oop.model.DungeonMap;
+import oop.model.SimulationOfFight;
+
+public class MenuPresenter {
+
+    @FXML
+    public Button startButton;
+
+    public void handleStartButtonAction(ActionEvent actionEvent) throws Exception {
+        Stage stage = new Stage();
+        DungeonMap simulation = new DungeonMap(stage);
+    }
+
+
+}
